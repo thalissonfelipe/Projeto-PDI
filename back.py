937faaafd594415384ecb5a7c00192adfbcb80a7
Thalisson <thalissonlipe7@gmail.com>
2019-09-29 07:57:00 -0300

    Códigos refatorados utilizando o flake8.

--- back.py
+++ back.py
@@ -1,295 +1,336 @@
 import imageio
-import cv2 #comparar histograma
+import util
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from colors import *
-from util import *
+from colors import rgb2gray
 plt.rcParams['figure.figsize'] = (15, 15)
 
+
+MAX_PIXEL = 255
+
+
 def negative_filter(image):
-	return 255 - image
+    return (MAX_PIXEL - image).astype(np.uint8)
 
-def logarithm_filter(image, c):
-	return c * np.log(1 + image)
 
-def gamma_filter(image, c, gama):
-	return c * np.power(image, gama)
+def logarithm_filter(image, c=1):
+    log = c * (np.log(image + 1) / (np.log(1 + np.max(image)))) * MAX_PIXEL
+    return log.astype(np.uint8)
 
-#TODO: implementar transformaçao linear
-#def linear_transform(i):
+
+def gamma_filter(image, gamma, c=1):
+    gamma = c * np.array(MAX_PIXEL * (image / MAX_PIXEL) ** gamma)
+    return gamma.astype(np.uint8)
+
+
+# TODO: implementar transformaçao linear
+# def linear_transform(i):
+
 
 def histogram(image, bins=256):
-	if len(image.shape) == 2: # 2D Grayscale Image
-		hist = np.zeros(bins, dtype='int')
-		flat = np.asarray(image)
-		flat = flat.flatten()
+    if len(image.shape) == 2:  # 2D Grayscale Image
+        hist = np.zeros(bins, dtype=np.int)
+        flat = np.asarray(image)
+        flat = flat.flatten()
 
-		for pxl in flat:
-			hist[pxl] += 1
+        for pxl in flat:
+            hist[pxl] += 1
 
-		return hist
-	else:					  # RGB Image
-		r, g, b = np.zeros(bins), np.zeros(bins), np.zeros(bins)
-		for v in range(image.shape[0]):
-			for w in range(image.shape[1]):
-				r[image[v,w][0]] += 1
-				g[image[v,w][1]] += 1
-				b[image[v,w][2]] += 1
+        return hist
+    else:  # RGB Image
+        r, g, b = np.zeros(bins), np.zeros(bins), np.zeros(bins)
+        for row in range(image.shape[0]):
+            for col in range(image.shape[1]):
+                r[image[row, col][0]] += 1
+                g[image[row, col][1]] += 1
+                b[image[row, col][2]] += 1
 
-		return (r, g, b) 
+        return (r, g, b)
+
 
 def equalize_hist(image, hist):
-	if len(image.shape) == 2:				## 2D Grayscale Image
-		y = cumulative_distribution(hist)
-		flat = (np.asarray(image)).flatten()
+    if len(image.shape) == 2:  # 2D Grayscale Image
+        y = util.cumulative_distribution(hist)
+        flat = (np.asarray(image)).flatten()
 
-		new_img = y[flat]
-		new_img = np.reshape(new_img, image.shape)
+        new_img = y[flat]
+        new_img = np.reshape(new_img, image.shape)
 
-		return new_img 
-	## TODO
-	else:									## RGB Image
-		cr, cg, cb = cumulative_distribution(hist)
-		flatr = (np.asarray(image[:,:,0])).flatten()
-		flatg = (np.asarray(image[:,:,1])).flatten()
-		flatb = (np.asarray(image[:,:,2])).flatten()
+        return new_img
+    # TODO
+    # else:  # RGB Image
+    #     cr, cg, cb = cumulative_distribution(hist)
+    #     flatr = (np.asarray(image[:,:,0])).flatten()
+    #     flatg = (np.asarray(image[:,:,1])).flatten()
+    #     flatb = (np.asarray(image[:,:,2])).flatten()
 
-		new_img = np.zeros_like(image)
-		print(len(new_img[:,:,0]), len(cr[flatr]))
-		new_img[:,:,0] = cr[flatr]
-		new_img[:,:,1] = cg[flatg]
-		new_img[:,:,2] = cb[flatb]
-		new_img = np.reshape(new_img, image.shape)
+    #     new_img = np.zeros_like(image)
+    #     print(len(new_img[:,:,0]), len(cr[flatr]))
+    #     new_img[:,:,0] = cr[flatr]
+    #     new_img[:,:,1] = cg[flatg]
+    #     new_img[:,:,2] = cb[flatb]
+    #     new_img = np.reshape(new_img, image.shape)
 
-		return new_img
-	
+    #     return new_img
+
+
 def conv(image, kernel):
-	height, width = image.shape
-	output = np.zeros_like(image)
-	kernel = np.flipud(np.fliplr(kernel))
-	pad = (kernel.shape[0] - 1) // 2
-	#image_padded = np.zeros((height + pad, width + pad))
-	#print(image_padded.shape)
-	#image_padded[pad:-pad, pad:-pad] = image
-	image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
+    height, width = image.shape
+    output = np.zeros_like(image)
+    kernel = np.flipud(np.fliplr(kernel))
+    pad = (kernel.shape[0] - 1) // 2
+    # image_padded = np.zeros((height + pad, width + pad))
+    # print(image_padded.shape)
+    # image_padded[pad:-pad, pad:-pad] = image
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
 
-	for v in np.arange(pad, height + pad):
-		for w in np.arange(pad, width + pad):
-			roi = image[v-pad:v+pad+1, w-pad:w+pad+1]
-			k = (roi * kernel)
-			output[v-pad, w-pad] = min(255, max(0, k.sum()))
+    for row in np.arange(pad, height + pad):
+        for col in np.arange(pad, width + pad):
+            roi = image[row-pad:row+pad+1, col-pad:col+pad+1]
+            k = (roi * kernel)
+            output[row-pad, col-pad] = min(MAX_PIXEL, max(0, k.sum()))
 
-	return output.astype('uint8')
+    return output.astype(np.uint8)
+
+
+def fft(x):
+    N = len(x)
+    if N <= 1:
+        return x
+    even = fft(x[0::2])
+    odd = fft(x[1::2])
+    T = [np.exp(-2j*np.pi*k/N)*odd[k] for k in range(N//2)]
+    return [even[k] + T[k] for k in range(N//2)] + \
+           [even[k] - T[k] for k in range(N//2)]
+
 
 """def conv(image, kernel):
-	height, width = image.shape
-	pad = (kernel.shape[0] - 1)
-	output = np.zeros_like(image)
+    height, width = image.shape
+    pad = (kernel.shape[0] - 1)
+    output = np.zeros_like(image)
 
-	for row in range(1, height - 1):
-		for col in range(1, width - 1):
-			k = kernel * image[(row-1):(row+2), (col-1):(col+2)]
-			output[row-1, col-1] = min(255, max(0, k.sum()))
+    for row in range(1, height - 1):
+        for col in range(1, width - 1):
+            k = kernel * image[(row-1):(row+2), (col-1):(col+2)]
+            output[row-1, col-1] = min(MAX_PIXEL, max(0, k.sum()))
 
-	return output.astype('uint8')"""
+    return output.astype(np.uint8)"""
+
 
 def mean_filter(image, filter_size):
-	f = np.ones((filter_size, filter_size))*(1.0/(filter_size**2))
-	return conv(image, f) 
+    f = np.ones((filter_size, filter_size))*(1.0/(filter_size**2))
+    return conv(image, f)
+
 
 def laplacian_filter(image, diagonal=True):
-	if diagonal:
-		f = np.array((
-			[-1, -1, -1],
-			[-1, 8, -1],
-			[-1, -1, -1]))	
-	else:
-		f = np.array((
-			[0, 1, 0],
-			[1, -4, 1],
-			[0, 1, 0]))
+    if diagonal:
+        f = np.array((
+            [-1, -1, -1],
+            [-1, 8, -1],
+            [-1, -1, -1]))
+    else:
+        f = np.array((
+            [0, 1, 0],
+            [1, -4, 1],
+            [0, 1, 0]))
 
-	return conv(image, f)
+    return conv(image, f)
+
 
 def sharpen_filter(image):
-	lap = laplacian_filter(image)
-	output = image + lap
-	output = (output * 255.0) / output.max()
-	return output.astype('uint8')
+    lap = laplacian_filter(image)
+    output = image + lap
+    output[np.where(output > MAX_PIXEL)] = MAX_PIXEL
+    # output = (output * MAX_PIXEL) / output.max()
+    return output.astype(np.uint8)
+
 
 def sobel_filter(image):
-	fx = np.array(([-1,0,1],[-2,0,2],[-1,0,1]))
-	fy = np.array(([-1,-2,-1],[0,0,0],[1,2,1]))
+    fx = np.array(([-1, 0, 1], [-2, 0, 2], [-1, 0, 1]))
+    fy = np.array(([-1, -2, -1], [0, 0, 0], [1, 2, 1]))
 
-	gx = conv(image, fx)
-	gy = conv(image, fy)
+    gx = conv(image, fx)
+    gy = conv(image, fy)
 
-	output = abs(gx) + abs(gy) #np.sqrt(gx ** 2 + gy ** 2) slower
-	output = (output * 255.0) / output.max()
-	return output.astype('uint8')
+    output = abs(gx) + abs(gy)  # np.sqrt(gx ** 2 + gy ** 2) slower
+    # output[np.where(output > MAX_PIXEL)] = MAX_PIXEL
+    output = (output * MAX_PIXEL) / output.max()
+    return output.astype(np.uint8)
+
 
 def median_filter(image, filter_size):
-	height, width = image.shape
-	mid = (filter_size - 1) // 2
-	output = np.zeros_like(image)
+    height, width = image.shape
+    mid = (filter_size - 1) // 2
+    output = np.zeros_like(image)
 
-	for v in range(height):
-		for w in range(width):
-			neighbors = []
-			for x in range(filter_size):
-				if v + x - mid < 0 or v + x - mid > height - 1:
-					for i in range(filter_size):
-						neighbors.append(0)
-				else:
-					if w + x - mid < 0 or w + mid > width - 1:
-						neighbors.append(0)
-					else:
-						for i in range(filter_size):
-							neighbors.append(image[v+x-mid,w+i-mid])
+    for row in range(height):
+        for col in range(width):
+            neighbors = []
+            for x in range(filter_size):
+                if row + x - mid < 0 or row + x - mid > height - 1:
+                    for i in range(filter_size):
+                        neighbors.append(0)
+                else:
+                    if col + x - mid < 0 or col + mid > width - 1:
+                        neighbors.append(0)
+                    else:
+                        for i in range(filter_size):
+                            neighbors.append(image[row+x-mid, col+i-mid])
 
-			neighbors.sort()
-			output[v,w] = neighbors[len(neighbors) // 2]
+            neighbors.sort()
+            output[row, col] = neighbors[len(neighbors) // 2]
 
-	output = (output * 255.) / output.max()
-	return output.astype('uint8')
+    output = (output * MAX_PIXEL) / output.max()
+    return output.astype(np.uint8)
+
 
 """def multiply(neighbors):
-	neighbors = np.array(neighbors, dtype=np.float64)
-	result = 1
-	for x in neighbors:
-		result *= x
-	return result
+    neighbors = np.array(neighbors, dtype=np.float64)
+    result = 1
+    for x in neighbors:
+        result *= x
+    return result
+
 
 ## Bordas pretas
 def geometric_mean_filter(image, filter_size):
-	height, width = image.shape
-	mid = filter_size // 2
-	output = np.zeros_like(image)
+    height, width = image.shape
+    mid = filter_size // 2
+    output = np.zeros_like(image)
 
-	for v in range(height):
-		for w in range(width):
-			neighbors = []
-			for x in range(filter_size):
-				if v + x - mid < 0 or v + x - mid > height - 1:
-					for i in range(filter_size):
-						neighbors.append(0)
-				else:
-					if w + x - mid < 0 or w + mid > width - 1:
-						neighbors.append(0)
-					else:
-						for i in range(filter_size):
-							neighbors.append(image[v+x-mid,w+i-mid])
+    for v in range(height):
+        for w in range(width):
+            neighbors = []
+            for x in range(filter_size):
+                if v + x - mid < 0 or v + x - mid > height - 1:
+                    for i in range(filter_size):
+                        neighbors.append(0)
+                else:
+                    if w + x - mid < 0 or w + mid > width - 1:
+                        neighbors.append(0)
+                    else:
+                        for i in range(filter_size):
+                            neighbors.append(image[v+x-mid,w+i-mid])
 
-			output[v,w] = np.power(multiply(neighbors), 1.0/(filter_size**2))
+            output[v,w] = np.power(multiply(neighbors), 1.0/(filter_size**2))
 
-	return output.astype('uint8')"""
+    return output.astype(np.uint8)"""
+
 
 def geometric_mean_filter(image, filter_size):
-	height, width = image.shape
-	pad = (filter_size - 1) // 2
-	output = np.zeros_like(image)
-	image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
+    height, width = image.shape
+    pad = (filter_size - 1) // 2
+    output = np.zeros_like(image)
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
 
-	for v in np.arange(pad, height + pad):
-		for w in np.arange(pad, width + pad):
-			kernel = image[v-pad:v+pad+1, w-pad:w+pad+1]
-			output[v-pad, w-pad] = geometric_mean_aux(kernel) ** \
-											(1./(filter_size**2))
+    for row in np.arange(pad, height + pad):
+        for col in np.arange(pad, width + pad):
+            kernel = image[row-pad:row+pad+1, col-pad:col+pad+1]
+            output[row-pad, col-pad] = (util.geometric_mean_aux(kernel)) ** \
+                                       (1./(filter_size**2))
 
-	output = (output * 255.0) / output.max()
-	return output.astype('uint8')
+    output = (output * MAX_PIXEL) / output.max()
+    return output.astype(np.uint8)
+
 
 def harmonic_mean_filter(image, filter_size):
-	height, width = image.shape
-	pad = (filter_size - 1) // 2
-	output = np.zeros_like(image)
-	image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
+    height, width = image.shape
+    pad = (filter_size - 1) // 2
+    output = np.zeros_like(image)
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
 
-	for v in np.arange(pad, height + pad):
-		for w in np.arange(pad, width + pad):
-			kernel = image[v-pad:v+pad+1, w-pad:w+pad+1]
-			s = harmonic_mean_aux(kernel)
-			if s == 0: # Check division by 0
-				output[v-pad, w-pad] = 0
-				continue
-			output[v-pad, w-pad] = (filter_size**2) / s
+    for row in np.arange(pad, height + pad):
+        for col in np.arange(pad, width + pad):
+            kernel = image[row-pad:row+pad+1, col-pad:col+pad+1]
+            s = util.harmonic_mean_aux(kernel)
+            if s == 0:  # Check division by 0
+                output[row-pad, col-pad] = 0
+                continue
+            output[row-pad, col-pad] = (filter_size**2) / s
 
-	output = (output * 255.0) / output.max()
-	return output.astype('uint8')
+    output = (output * MAX_PIXEL) / output.max()
+    return output.astype(np.uint8)
+
 
 def contraharmonic_mean_filter(image, filter_size):
-	height, width = image.shape
-	pad = (filter_size - 1) // 2
-	output = np.zeros_like(image)
-	image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
+    height, width = image.shape
+    pad = (filter_size - 1) // 2
+    output = np.zeros_like(image)
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_DEFAULT)
 
-	for v in np.arange(pad, height + pad):
-		for w in np.arange(pad, width + pad):
-			kernel = image[v-pad:v+pad+1, w-pad:w+pad+1]
-			output[v-pad, w-pad] = contraharmonic_mean_aux(kernel)
+    for row in np.arange(pad, height + pad):
+        for col in np.arange(pad, width + pad):
+            kernel = image[row-pad:row+pad+1, col-pad:col+pad+1]
+            output[row-pad, col-pad] = util.contraharmonic_mean_aux(kernel)
 
-	output = (output * 255.0) / output.max()
-	return output.astype('uint8')
+    output = (output * MAX_PIXEL) / output.max()
+    return output.astype(np.uint8)
+
 
 def sepia_filter(image):
-	img = np.asarray(image)
+    img = np.asarray(image)
 
-	lmap = np.matrix([[ 0.393, 0.769, 0.189 ],
-					  [ 0.349, 0.686, 0.168 ],
-					  [ 0.272, 0.534, 0.131 ]])
-	
-	output = np.array([x * lmap.T for x in img])
-	output[np.where(output > 255)] = 255
-	output = np.reshape(output, image.shape)
+    lmap = np.matrix([[0.393, 0.769, 0.189],
+                      [0.349, 0.686, 0.168],
+                      [0.272, 0.534, 0.131]])
 
-	return output.astype('uint8')
+    output = np.array([x * lmap.T for x in img])
+    output[np.where(output > MAX_PIXEL)] = MAX_PIXEL
+    output = np.reshape(output, image.shape)
+
+    return output.astype(np.uint8)
+
 
 """ Demora uns 20 segundos
 def sepia_filter2(image):
-	height, width, _ = image.shape
-	output = np.zeros_like(image)
+    height, width, _ = image.shape
+    output = np.zeros_like(image)
 
-	for v in range(height):
-		for w in range(width):
-			r, g, b = image[v,w]
-			tr = int(0.393 * r + 0.769 * g + 0.189 * b)
-			tg = int(0.349 * r + 0.686 * g + 0.168 * b)
-			tb = int(0.272 * r + 0.534 * g + 0.131 * b)
+    for v in range(height):
+        for w in range(width):
+            r, g, b = image[v,w]
+            tr = int(0.393 * r + 0.769 * g + 0.189 * b)
+            tg = int(0.349 * r + 0.686 * g + 0.168 * b)
+            tb = int(0.272 * r + 0.534 * g + 0.131 * b)
 
-			if tr > 255: tr = 255
-			if tg > 255: tg = 255
-			if tb > 255: tb = 255
+            if tr > MAX_PIXEL: tr = MAX_PIXEL
+            if tg > MAX_PIXEL: tg = MAX_PIXEL
+            if tb > MAX_PIXEL: tb = MAX_PIXEL
 
-			output[v,w] = tr, tg, tb
+            output[v,w] = tr, tg, tb
 
-	return output.astype('uint8')"""
+    return output.astype(np.uint8)"""
+
 
 if __name__ == '__main__':
-	i = imageio.imread('images/breast.jpeg')
-	i = rgb2gray(i)
-	#hist = histogram(i)
-	#it = equalize_hist(i, hist)
-	#r, g, b = histogram(i, 256)
-	#cr, cg, cb = cumulative_distribution((r, g, b))
-	#it = equalize_hist(i, (r, g, b))
-	#plt.plot(cr, color='r')
-	#plt.plot(cg, color='g')
-	#plt.plot(cb, color='b')
-	#plt.show()
-	#it = equalize_hist(i, hist)
-	#histeq = histogram(it, 256)
-	#subplot_hist(hist, histeq)
-	#subplot_img(i, it)
-	#subplot(histogram(i, 256), histogram(it, 256))
-	#it = mean_filter(i, 35)
-	#it = sepia_filter(i)
-	#it = laplacian_filter(i)
-	#it = sharpen_filter(i)
-	it = negative_filter(i)
-	#hist = histogram(it)
-	#it = equalize_hist(it, hist)
-	#it = sobel_filter(i)
-	#it = geometric_mean_filter(i, 3)
-	#it = harmonic_mean_filter(i, 3)
-	#it = contraharmonic_mean_filter(i, 3)
-	subplot_img(i, it)
+    i = imageio.imread('images/einstein.jpeg')
+    i = rgb2gray(i)
+    # it = logarithm_filter(i, 1)
+    # hist = histogram(i)
+    # it = equalize_hist(i, hist)
+    # r, g, b = histogram(i, 256)
+    # cr, cg, cb = cumulative_distribution((r, g, b))
+    # it = equalize_hist(i, (r, g, b))
+    # plt.plot(cr, color='r')
+    # plt.plot(cg, color='g')
+    # plt.plot(cb, color='b')
+    # plt.show()
+    # it = equalize_hist(i, hist)
+    # histeq = histogram(it, 256)
+    # subplot_hist(hist, histeq)
+    # subplot_img(i, it)
+    # subplot(histogram(i, 256), histogram(it, 256))
+    # it = mean_filter(i, 35)
+    # it = sepia_filter(i)
+    # it = laplacian_filter(i)
+    # it = gamma_filter(i, 0.4)
+    # it = sharpen_filter(i)
+    # it = negative_filter(i)
+    # hist = histogram(it)
+    # it = equalize_hist(it, hist)
+    # it = sobel_filter(i)
+    # it = geometric_mean_filter(i, 3)
+    # it = harmonic_mean_filter(i, 3)
+    # it = contraharmonic_mean_filter(i, 3)
+    # util.subplot_img(i, it)
--- colors.py
+++ colors.py
@@ -1,66 +1,71 @@
 import math
 import numpy as np
 
+
 def rgb2gray(image):
-	if len(image.shape) == 2:
-		return image
-	else:
-		gray = np.dot(image[...,:3], [0.298936, 0.587043, 0.114021])
-		igray = np.zeros_like(gray, dtype=int)
-		for v in range(igray.shape[0]):
-			for w in range(igray.shape[1]):
-				igray[v][w] = int(round(gray[v][w],5))
-		return igray
+    if len(image.shape) == 2:  # Check if image is already grayscale
+        return image
+    else:
+        return np.dot(image[..., :3], [0.298936, 0.587043, 0.114021])
 
-## Convert RGB image to grayscale using weighted average
-#def rgb2gray(r, g, b):
-#	return (r * 0.298936) + (g * 0.587043) + (b * 0.114021)
+# Convert RGB image to grayscale using weighted average
+# def rgb2gray(r, g, b):
+#     return (r * 0.298936) + (g * 0.587043) + (b * 0.114021)
 
-## Convert RGB image to grayscale using arithmetic average
+
+# Convert RGB image to grayscale using arithmetic average
 def rgb2gray_avg(r, g, b):
-	return (r + g + b) / 3
+    return (r + g + b) / 3
 
-## Convert RGB colors to HSV
+
+# Convert RGB colors to HSV
 def rgb2hsv(r, g, b):
-	r, g, b = r / 255.0, g / 255.0, b / 255.0
-	h, s, v = 0, 0, 0
-	maximo = max(r, g, b)
-	minimo = min(r, g, b)
-	dif = maximo - minimo
+    r, g, b = r / 255.0, g / 255.0, b / 255.0
+    h, s, v = 0, 0, 0
+    maximo = max(r, g, b)
+    minimo = min(r, g, b)
+    dif = maximo - minimo
 
-	if maximo == minimo:
-		h = 0
-	elif maximo == r:
-		h = (60 * ((g - b) / dif) + 360) % 360
-	elif maximo == g:
-		h = (60 * ((b - r) / dif) + 120) % 360
-	elif maximo == b:
-		s = 0
-	else:
-		s = (dif / maximo) * 100
-	v = maximo * 100
+    if maximo == minimo:
+        h = 0
+    elif maximo == r:
+        h = (60 * ((g - b) / dif) + 360) % 360
+    elif maximo == g:
+        h = (60 * ((b - r) / dif) + 120) % 360
+    elif maximo == b:
+        s = 0
+    else:
+        s = (dif / maximo) * 100
+        v = maximo * 100
 
-	return (h, s, v)
+    return (h, s, v)
 
-## Convert HSV colors to RGB
+
+# Convert HSV colors to RGB
 def hsv2rgb(h, s, v):
-	h, s, v = float(h), float(s), float(v)
-	h60 = h / 60.0
-	h60f = math.floor(h60)
-	hi = int(h60f) % 6
-	f = h60 - h60f
-	p,q,t = int(255*v*(1.-s)), int(255*v*(1.-s*f)), int(255*v*(1.-s*(1.-f))) 
-	v = int(255*v)
-	if hi == 0: return (v, t, p)
-	if hi == 1: return (q, v, p)
-	if hi == 2: return (p, v, t)
-	if hi == 3: return (p, q, v)
-	if hi == 4: return (t, p, v)
-	if hi == 5: return (v, p, q)
+    h, s, v = float(h), float(s), float(v)
+    h60 = h / 60.0
+    h60f = math.floor(h60)
+    hi = int(h60f) % 6
+    f = h60 - h60f
+    p, q, t = int(255*v*(1.-s)), int(255*v*(1.-s*f)), int(255*v*(1.-s*(1.-f)))
+    v = int(255*v)
+    if hi == 0:
+        return (v, t, p)
+    if hi == 1:
+        return (q, v, p)
+    if hi == 2:
+        return (p, v, t)
+    if hi == 3:
+        return (p, q, v)
+    if hi == 4:
+        return (t, p, v)
+    if hi == 5:
+        return (v, p, q)
 
 
 if __name__ == '__main__':
-	print(rgb2hsv(255, 0, 0)) #red
-	print(rgb2hsv(0, 255, 0)) #green
-	print(hsv2rgb(240, 1, 1)) #blue
-	print(hsv2rgb(300, 1, 1)) #magenta
+    print(rgb2hsv(255, 0, 0))  # red
+    print(rgb2hsv(0, 255, 0))  # green
+    print(hsv2rgb(240, 1, 1))  # blue
+    print(hsv2rgb(300, 1, 1))  # magenta
